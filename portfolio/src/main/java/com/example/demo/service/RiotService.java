c9b2067288a9c5beb0add2e052c5be07f59ad072
USER <USER@host.docker.internal>
2021-11-22 17:39:46 +0900

    랭크 타입별 결과 프론트 전달 소스 구현

--- portfolio/src/main/java/com/example/demo/service/RiotService.java
+++ portfolio/src/main/java/com/example/demo/service/RiotService.java
@@ -5,100 +5,25 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class RiotService {
-	String api_key = "RGAPI-391dd36e-13cb-4370-9c69-606c9fe9a16f";
 	public void search(String name, Model model) {
-		// 검색한 아이디로 api 호출
-		// name 값 받아 와 한글로 인코딩 후 소환사 검색 api 출력  
-		// https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%EB%A7%88%EC%8A%A4%ED%84%B0%EA%B0%84%EB%8B%A4%EC%A0%95%EC%98%81%ED%9B%88?api_key=RGAPI-391dd36e-13cb-4370-9c69-606c9fe9a16f
-		// domain = https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key=
-		
-		// response에서 id 값 받아 와 전적검색 api 출력 
-		// https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/ZNpxZhNhXW77QeRkX3BoPEAHsvm9USDX7vzUSkeo1foo-w?api_key=RGAPI-391dd36e-13cb-4370-9c69-606c9fe9a16f
-		
-		// domain = https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{id}?api_key=
 		String id = null;
-		 try {
-	            StringBuilder urlBuilder = new StringBuilder("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + URLEncoder.encode(name, "UTF-8") + "?api_key=" + api_key); 
-	            URL url = new URL(urlBuilder.toString());
-	            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-	            con.setRequestMethod("GET");
-	            int responseCode = con.getResponseCode();
-	            BufferedReader br;
-	            if(responseCode==200) { // 정상 호출
-	                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-	            } else {  // 에러 발생
-	                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-	            }
-	            String inputLine;
-	            StringBuffer response = new StringBuffer();
-	            while ((inputLine = br.readLine()) != null) {
-	                response.append(inputLine);
-	            }
-	            br.close();
-	            System.out.println(response.toString());
-	            
-	            JSONParser json = new JSONParser();
-	            JSONObject object = (JSONObject)json.parse(response.toString());
-	            id = object.get("id").toString(); // 전적 검색 때 사용할 아이디
-	            String accountId = object.get("accountId").toString(); 
-	            String puuid = object.get("puuid").toString();
-	            Long profileIconId = (Long) object.get("profileIconId");
-	            Long revisionDate = (Long) object.get("revisionDate");
-	            Long summonerLevel = (Long) object.get("summonerLevel");
-	        } catch (Exception e) {
-	            System.out.println(e);
-	        }
-		 
-		 try {
-	            StringBuilder urlBuilder = new StringBuilder("https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/" + URLEncoder.encode(id, "UTF-8") + "?api_key=" + api_key); 
-	            URL url = new URL(urlBuilder.toString());
-	            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-	            con.setRequestMethod("GET");
-	            int responseCode = con.getResponseCode();
-	            BufferedReader br;
-	            if(responseCode==200) { // 정상 호출
-	                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-	            } else {  // 에러 발생
-	                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-	            }
-	            String inputLine;
-	            StringBuffer response = new StringBuffer();
-	            while ((inputLine = br.readLine()) != null) {
-	                response.append(inputLine);
-	            }
-	            br.close();
-	            System.out.println(response.toString());
-	            
-	            JSONParser json = new JSONParser();
-	            JSONObject object = (JSONObject)json.parse(response.toString());
-	            System.out.println(object.toString());
-	        } catch (Exception e) {
-	            System.out.println(e);
-	        }
-		 
-	}
-	public static void main(String[] args) {
-		String api_key = "RGAPI-391dd36e-13cb-4370-9c69-606c9fe9a16f";
-		String name ="마스터간다정영훈";
-		// 검색한 아이디로 api 호출
-		// name 값 받아 와 한글로 인코딩 후 소환사 검색 api 출력  
-		// https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%EB%A7%88%EC%8A%A4%ED%84%B0%EA%B0%84%EB%8B%A4%EC%A0%95%EC%98%81%ED%9B%88?api_key=RGAPI-391dd36e-13cb-4370-9c69-606c9fe9a16f
-		// domain = https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/{name}?api_key=
-		
-		// response에서 id 값 받아 와 전적검색 api 출력 
-		// https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/ZNpxZhNhXW77QeRkX3BoPEAHsvm9USDX7vzUSkeo1foo-w?api_key=RGAPI-391dd36e-13cb-4370-9c69-606c9fe9a16f
-		
-		// domain = https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{id}?api_key=
-		String id = null;
+		String api_key = "RGAPI-e374823f-86a8-4dd7-a733-ed3e08ed5886";
 		 try {
 	            StringBuilder urlBuilder = new StringBuilder("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + URLEncoder.encode(name, "UTF-8") + "?api_key=" + api_key); 
 	            URL url = new URL(urlBuilder.toString());
@@ -147,10 +72,77 @@ public class RiotService {
 	            while ((inputLine = br.readLine()) != null) {
 	                response.append(inputLine);
 	            }
-	            br.close();
+	            JSONParser json = new JSONParser();
+	            JSONArray object = (JSONArray)json.parse(response.toString());
+	            ObjectMapper objectMapper = new ObjectMapper();
+		        Map<String, Object> map = new HashMap<>();	            
+	            switch (object.size()) {
+				case 0:
+					model.addAttribute("code","0");
+					model.addAttribute("result", "등록되지 않은 소환사입니다. 오타를 확인 후 다시 검색해주세요.");
+					break;
+				case 1:
+			        map = objectMapper.readValue(object.toString(), new TypeReference<Map<String, Object>>(){});
+			        	if(map.get("queueType").equals("RANKED_FLEX_SR")) {
+			        		model.addAttribute("code","solo");
+							model.addAttribute("tier_free", (String) map.get("tier"));
+							model.addAttribute("rank_free", (String) map.get("rank"));
+							model.addAttribute("leaguePoints_free", (int) map.get("leaguePoints"));
+							model.addAttribute("wins_free", (int) map.get("wins"));
+			        	}else if(map.get("queueType").equals("RANKED_SOLO_5x5")){
+			        		model.addAttribute("code","free");
+							model.addAttribute("tier_solo", (String) map.get("tier"));
+							model.addAttribute("rank_solo", (String) map.get("rank"));
+							model.addAttribute("leaguePoints_solo", (int) map.get("leaguePoints"));
+							model.addAttribute("wins_solo", (int) map.get("wins"));
+			        	}
+					break;
+				case 2:
+					model.addAttribute("code","2");
+			        List<Map<String, Object>> list = new ArrayList<>();
+			        list = objectMapper.readValue(object.toString(), new TypeReference<List<Map<String, Object>>>(){});
+			        for(int i=0; i<2; i++) {
+			        	if(list.get(i).get("queueType").equals("RANKED_FLEX_SR")){
+			        		model.addAttribute("code","solo");
+							model.addAttribute("tier_free", (String) map.get("tier"));
+							model.addAttribute("rank_free", (String) map.get("rank"));
+							model.addAttribute("leaguePoints_free", (int) map.get("leaguePoints"));
+							model.addAttribute("wins_free", (int) map.get("wins"));
+			        	}else if(list.get(i).get("queueType").equals("RANKED_SOLO_5x5")) {
+			        		model.addAttribute("code","free");
+							model.addAttribute("tier_solo", (String) map.get("tier"));
+							model.addAttribute("rank_solo", (String) map.get("rank"));
+							model.addAttribute("leaguePoints_solo", (int) map.get("leaguePoints"));
+							model.addAttribute("wins_solo", (int) map.get("wins"));
+			        	}
+			        }
+			        break;
+			    default :
+			    	model.addAttribute("code","error");
+			    	model.addAttribute("result", "알 수 없는 오류가 발생했습니다. 잠시 후 다시 검색해주세요");
+					break;
+				}
+	            
 	            System.out.println(response.toString());
 	        } catch (Exception e) {
 	            System.out.println(e);
 	        }
 	}
 }
+//널처리
+//
+//이름 없을 때 - 등록되지 않은 소환사입니다. 오타를 확인 후 다시 검색해주세요.
+//
+//이름 있을 때 - 검색 
+//
+//랭크 없을 때
+//둘다 있을 때  
+//솔로랭크만 있을 때
+//자유랭크만 있을 때
+//
+//전적 = 승/(승+패) 
+
+//참고할주소
+//https://github.com/HamSungJun/FM.GG
+//https://developer.riotgames.com/docs/lol#data-dragon_champions
+//https://developer.riotgames.com/apis#match-v5/GET_getTimeline 전적결과 match-5
